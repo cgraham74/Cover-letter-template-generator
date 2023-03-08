48166923f0798a5d5040c191156158af7b169aa2
ChristinaGraham <cgraham74@gmail.com>
2023-03-07 19:59:47 -0600

    Modal implemented and template refactored

--- popup.js
+++ popup.js
@@ -1,12 +1,9 @@
 const coverLetterHeader = document.getElementById("cover-letter-title-el");
 const downloadDocBtn = document.getElementById("download-doc-btn");
 const downloadPdfBtn = document.getElementById("download-pdf-btn");
-//Prompt questions - 
-//put form in a modal
+
 //blur background of popup
-//Save form data to session storage (do not want it to persist like the other data)
-//insert data into cover letter
-//call render popup
+
 
 //Data needed / questions to ask
 //completed program / What course did you complete?
@@ -14,7 +11,6 @@ const downloadPdfBtn = document.getElementById("download-pdf-btn");
 //Three job bullets / What bullet points are in the job listing?
 //Recruiter name (if blank - use company name) / Is there a hiring person/recruiter to send cover letter to?
 //Job title you are applying to / What is the position title?
-
 
 //Implement download as word doc functionality
 
@@ -29,14 +25,9 @@ function getCurrentLocalDate() {
   return localDate;
 }
 
-const company = {
-  name: "Acme",
-  hiringPerson: "Chuck Jones",
-  jobTitle: "Super Genius",
-  firstBullet: "Beta Tester",
-  secondBullet: "Scientist",
-  thirdBullet: "Product Designer",
-};
+//create company outside of modal for template access
+const company = {};
+
 const user = {
   fullname: "",
   email: "",
@@ -71,6 +62,94 @@ const projects = [
 
 let softskills = [];
 
+//------------beginning of modal-----------------------
+
+// Create the modal element
+const modal = document.createElement("div");
+modal.classList.add("modal");
+
+// Create the content of the modal
+const content = document.createElement("div");
+content.classList.add("modal-content");
+
+// Create the input fields and labels
+const companyLabel = document.createElement("label");
+companyLabel.innerText = "Company Name";
+const companyInput = document.createElement("input");
+companyInput.type = "text";
+
+const hiringPersonLabel = document.createElement("label");
+hiringPersonLabel.innerText = "Hiring Person";
+const hiringPersonInput = document.createElement("input");
+hiringPersonInput.type = "text";
+
+const jobTitleLabel = document.createElement("label");
+jobTitleLabel.innerText = "Job Title";
+const jobTitleInput = document.createElement("input");
+jobTitleInput.type = "text";
+
+const firstBulletLabel = document.createElement("label");
+firstBulletLabel.innerText = "First Bullet Point";
+const firstBulletInput = document.createElement("input");
+firstBulletInput.type = "text";
+
+const secondBulletLabel = document.createElement("label");
+secondBulletLabel.innerText = "Second Bullet Point";
+const secondBulletInput = document.createElement("input");
+secondBulletInput.type = "text";
+
+const thirdBulletLabel = document.createElement("label");
+thirdBulletLabel.innerText = "Third Bullet Point";
+const thirdBulletInput = document.createElement("input");
+thirdBulletInput.type = "text";
+
+// Create the submit button
+const submitButton = document.createElement("button");
+submitButton.innerText = "Submit";
+
+// Add the input fields, labels, and submit button to the content div
+content.appendChild(companyLabel);
+content.appendChild(companyInput);
+content.appendChild(hiringPersonLabel);
+content.appendChild(hiringPersonInput);
+content.appendChild(jobTitleLabel);
+content.appendChild(jobTitleInput);
+content.appendChild(firstBulletLabel);
+content.appendChild(firstBulletInput);
+content.appendChild(secondBulletLabel);
+content.appendChild(secondBulletInput);
+content.appendChild(thirdBulletLabel);
+content.appendChild(thirdBulletInput);
+content.appendChild(submitButton);
+
+// Add the content div to the modal
+modal.appendChild(content);
+
+// Add the modal to the document
+document.body.appendChild(modal);
+
+// Create a function to handle the form submission
+function handleSubmit(event) {
+  event.preventDefault();
+
+  (company.name = companyInput.value),
+    (company.hiringPerson = hiringPersonInput.value),
+    (company.jobTitle = jobTitleInput.value),
+    (company.firstBullet = firstBulletInput.value),
+    (company.secondBullet = secondBulletInput.value),
+    (company.thirdBullet = thirdBulletInput.value),
+    console.log(company);
+
+  renderCover();
+  // Close the modal
+  modal.remove();
+}
+
+// Add an event listener to the submit button
+submitButton.addEventListener("click", handleSubmit);
+
+//-------------end of modal----------------------
+
 function greeting() {
   return company.hiringPerson ? company.hiringPerson : company.name;
 }
@@ -83,106 +162,106 @@ chrome.storage.local.get(null, function (items) {
   });
 });
 
-//turn into function to be called when ready to populate template
-const chromeObj = chrome.storage.local.get(
-  [
-    "first name",
-    "last name",
-    "mobile",
-    "email",
-    "LinkedIn",
-    "GitHub",
-    "portfolio",
-    "employed",
-    "employer",
-    "years",
-    "industry",
-    "job type",
-    "first soft skill",
-    "second soft skill",
-    "third soft skill",
-    "fourth soft skill",
-    "fifth soft skill",
-    "sixth soft skill",
-    "first project",
-    "first project description",
-    "second project",
-    "second project description",
-    "third project",
-    "third project description",
-  ],
-  function (result) {
-    if (chrome.runtime.lastError) {
-      console.log(chrome.runtime.lastError.message);
-      return;
+function renderCover() {
+  chrome.storage.local.get(
+    [
+      "first name",
+      "last name",
+      "mobile",
+      "email",
+      "LinkedIn",
+      "GitHub",
+      "portfolio",
+      "employed",
+      "employer",
+      "years",
+      "industry",
+      "job type",
+      "first soft skill",
+      "second soft skill",
+      "third soft skill",
+      "fourth soft skill",
+      "fifth soft skill",
+      "sixth soft skill",
+      "first project",
+      "first project description",
+      "second project",
+      "second project description",
+      "third project",
+      "third project description",
+    ],
+    function (result) {
+      if (chrome.runtime.lastError) {
+        console.log(chrome.runtime.lastError.message);
+        return;
+      }
+      console.log(result);
+      const {
+        "first name": firstname,
+        "last name": lastname,
+        mobile: mobile,
+        email: email,
+        LinkedIn: linkedin,
+        GitHub: github,
+        portfolio: portfolio,
+        employed: employed,
+        employer: employer,
+        years: years,
+        industry: industry,
+        "job type": jobtype,
+        "first soft skill": firstSkill,
+        "second soft skill": secondSkill,
+        "third soft skill": thirdSkill,
+        "fourth soft skill": fourthSkill,
+        "fifth soft skill": fifthSkill,
+        "sixth soft skill": sixthSkill,
+        "first project": firstProject,
+        "first project description": firstDesc,
+        "second project": secondProject,
+        "second project description": secondDesc,
+        "third project": thirdProject,
+        "third project description": thirdDesc,
+      } = result;
+
+      currentEmployer.name = employer;
+      currentEmployer.employed = employed;
+      currentEmployer.industry = industry;
+      currentEmployer.type = jobtype;
+      currentEmployer.years = years;
+
+      softskills = [
+        firstSkill,
+        secondSkill,
+        thirdSkill,
+        fourthSkill,
+        fifthSkill,
+        sixthSkill,
+      ];
+
+      user.fullname = firstname + " " + lastname;
+      user.mobile = mobile;
+      user.email = email;
+      user.linkedIn = linkedin;
+      user.gitHub = github;
+      user.portfolio = portfolio;
+
+      projects[0].name = firstProject;
+      projects[0].desc = firstDesc;
+      projects[1].name = secondProject;
+      projects[1].desc = secondDesc;
+      projects[2].name = thirdProject;
+      projects[2].desc = thirdDesc;
+
+      console.dir(projects);
+      console.dir(user);
+      console.dir(employer);
+      console.dir(company);
+      coverLetterHeader.innerText = `Cover letter for ${company.name}`;
+      coverLetterTemplate();
     }
-    console.log(result);
-    const {
-      "first name": firstname,
-      "last name": lastname,
-      mobile: mobile,
-      email: email,
-      LinkedIn: linkedin,
-      GitHub: github,
-      portfolio: portfolio,
-      employed: employed,
-      employer: employer,
-      years: years,
-      industry: industry,
-      "job type": jobtype,
-      "first soft skill": firstSkill,
-      "second soft skill": secondSkill,
-      "third soft skill": thirdSkill,
-      "fourth soft skill": fourthSkill,
-      "fifth soft skill": fifthSkill,
-      "sixth soft skill": sixthSkill,
-      "first project": firstProject,
-      "first project description": firstDesc,
-      "second project": secondProject,
-      "second project description": secondDesc,
-      "third project": thirdProject,
-      "third project description": thirdDesc,
-    } = result;
-
-    currentEmployer.name = employer;
-    currentEmployer.employed = employed;
-    currentEmployer.industry = industry;
-    currentEmployer.type = jobtype;
-    currentEmployer.years = years;
-
-    softskills = [
-      firstSkill,
-      secondSkill,
-      thirdSkill,
-      fourthSkill,
-      fifthSkill,
-      sixthSkill,
-    ];
-
-    user.fullname = firstname + " " + lastname;
-    user.mobile = mobile;
-    user.email = email;
-    user.linkedIn = linkedin;
-    user.gitHub = github;
-    user.portfolio = portfolio;
-
-    projects[0].name = firstProject;
-    projects[0].desc = firstDesc;
-    projects[1].name = secondProject;
-    projects[1].desc = secondDesc;
-    projects[2].name = thirdProject;
-    projects[2].desc = thirdDesc;
-
-    console.dir(projects);
-    console.dir(user);
-    console.dir(employer);
-    console.dir(company);
-    coverLetterHeader.innerText = `Cover letter for ${company.name}`;
-    renderCoverLetter();
-  }
-);
-
-function renderCoverLetter() {
+  );
+}
+function coverLetterTemplate() {
   const coverLetterContainer = document.getElementById(
     "cover-letter-container"
   );
